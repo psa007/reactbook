fa646a14fdeb8150e1917cb4f658fbd422b6c15d
Samuel A.A <wolewells@gmail.com>
2019-02-24 19:25:45 +0100

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Reactbook</h1>
+          <p>A simple profile taking app</p>
+        </div>
+      </div>
+    );
+  }
+}
